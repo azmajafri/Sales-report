9ea9134d0dba4d4df28c7daf6032989a4f59f44c
Azma Melia Jafri <61407560+azmajafri@users.noreply.github.com>
2024-01-31 18:23:53 +0800

    Add files via upload

---
+++ Sales/Data Cleaning & transformation/DIM_customer.sql
@@ -0,0 +1,41 @@
+-- Cleansed DIM_CUstomer Table --
+SELECT 
+  c.customerkey AS CustomerKey, 
+  --      [GeographyKey]
+  --      ,[CustomerAlternateKey]
+  --      ,[Title]
+  c.firstname AS [First Name], 
+  --      ,[MiddleName]
+  c.lastname AS [Last Name], 
+  c.firstname + ' ' + lastname AS [Full Name], 
+  -- Combined First and Last Name --
+  --      ,[NameStyle]
+  --      ,[BirthDate]
+  --      ,[MaritalStatus]
+  --      ,[Suffix]
+  CASE c.gender WHEN 'M' THEN 'Male' WHEN 'F' THEN 'Female' End AS Gender, 
+  --      ,[EmailAddress]
+  --      ,[YearlyIncome]
+  --      ,[TotalChildren]
+  --      ,[NumberChildrenAtHome]
+  --      ,[EnglishEducation]
+  --      ,[SpanishEducation]
+  --      ,[FrenchEducation]
+  --      ,[EnglishOccupation]
+  --      ,[SpanishOccupation]
+  --      ,[FrenchOccupation]
+  --      ,[HouseOwnerFlag]
+  --      ,[NumberCarsOwned]
+  --      ,[AddressLine1]
+  --      ,[AddressLine2]
+  --      ,[Phone]
+  c.datefirstpurchase AS [DateFirstPurchase], 
+  --      [CommuteDistance]
+  g.city AS [Customer City] -- Joined in customer city from Geography Table --
+FROM 
+  dbo.dimcustomer AS c 
+  LEFT JOIN dbo.dimgeography AS g on g.geographykey = c.geographykey 
+ORDER BY 
+  CustomerKey ASC -- Order List by CustomerKey
+
+  
